eca50a23083916632b43e59a87b6d98649c507c4
sahma19 <shd@energinet.dk>
2024-12-15 21:23:02 +0100

    Update unittest.go (#15)

--- pkg/tests/unittest.go
+++ pkg/tests/unittest.go
@@ -12,67 +12,101 @@ import (
 
 func RunUnitTests(testFiles []string) error {
 	var originalRules []*filenameAndData
+
 	for _, testFile := range testFiles {
-		b, err := os.ReadFile(testFile)
+		unitTestInp, err := parseTestFile(testFile)
 		if err != nil {
 			return err
 		}
 
-		var unitTestInp unitTestFile
-		unmarshalErr := yaml.Unmarshal(b, &unitTestInp)
-		if unmarshalErr != nil {
-			return unmarshalErr
-		}
-
 		for _, rulesFile := range unitTestInp.RuleFiles {
-			relativeRulesFile := fmt.Sprintf("%s/%s", filepath.Dir(testFile), rulesFile)
-
-			yamlFile, err := os.ReadFile(relativeRulesFile)
-			if err != nil {
+			if err := processRulesFile(testFile, rulesFile, &originalRules); err != nil {
 				return err
-			}
-
-			unstructured := make(map[interface{}]interface{})
-			unmarshalErr := yaml.Unmarshal(yamlFile, &unstructured)
-			if unmarshalErr != nil {
-				return unmarshalErr
-			}
-
-			if spec, found := unstructured["spec"]; found {
-				ruleFileContentWithoutMetadata, marshalErr := yaml.Marshal(spec)
-				if marshalErr != nil {
-					return marshalErr
-				}
-
-				originalRules = append(originalRules, &filenameAndData{relativeRulesFile, yamlFile})
-
-				writeErr := os.WriteFile(relativeRulesFile, ruleFileContentWithoutMetadata, 0o600)
-				if writeErr != nil {
-					return writeErr
-				}
-			} else {
-				log.Printf("No spec found in file %s", rulesFile)
 			}
 		}
 	}
 
-	promtoolArgs := append([]string{"test", "rules"}, testFiles...)
-	command := exec.Command("promtool", promtoolArgs...)
-	output, err := command.CombinedOutput()
-	if err != nil {
-		log.Printf("%s", output)
+	if err := runPromtoolTests(testFiles); err != nil {
 		restoreOriginalFiles(originalRules)
 		return err
 	}
-	log.Printf("%s", output)
+
 	restoreOriginalFiles(originalRules)
 	return nil
 }
 
+func parseTestFile(testFile string) (*unitTestFile, error) {
+	data, err := os.ReadFile(testFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var unitTestInp unitTestFile
+	if err := yaml.Unmarshal(data, &unitTestInp); err != nil {
+		return nil, err
+	}
+
+	return &unitTestInp, nil
+}
+
+func processRulesFile(testFile, rulesFile string, originalRules *[]*filenameAndData) error {
+	relativePath := fmt.Sprintf("%s/%s", filepath.Dir(testFile), rulesFile)
+
+	yamlData, err := os.ReadFile(relativePath)
+	if err != nil {
+		return err
+	}
+
+	unstructured, err := unmarshalYAML(yamlData)
+	if err != nil {
+		return err
+	}
+
+	if spec, found := unstructured["spec"]; found {
+		if err := updateRulesFile(relativePath, yamlData, spec, originalRules); err != nil {
+			return err
+		}
+	} else {
+		log.Printf("No spec found in file %s", rulesFile)
+	}
+
+	return nil
+}
+
+func unmarshalYAML(data []byte) (map[interface{}]interface{}, error) {
+	unstructured := make(map[interface{}]interface{})
+	if err := yaml.Unmarshal(data, &unstructured); err != nil {
+		return nil, err
+	}
+	return unstructured, nil
+}
+
+func updateRulesFile(filePath string, originalData []byte, spec interface{}, originalRules *[]*filenameAndData) error {
+	ruleFileContentWithoutMetadata, err := yaml.Marshal(spec)
+	if err != nil {
+		return err
+	}
+
+	*originalRules = append(*originalRules, &filenameAndData{filePath, originalData})
+
+	if err := os.WriteFile(filePath, ruleFileContentWithoutMetadata, 0o600); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func runPromtoolTests(testFiles []string) error {
+	promtoolArgs := append([]string{"test", "rules"}, testFiles...)
+	command := exec.Command("promtool", promtoolArgs...)
+	output, err := command.CombinedOutput()
+	log.Printf("%s", output)
+	return err
+}
+
 func restoreOriginalFiles(rules []*filenameAndData) {
 	for _, nameAndData := range rules {
-		err := os.WriteFile(nameAndData.filename, nameAndData.data, 0o600)
-		if err != nil {
+		if err := os.WriteFile(nameAndData.filename, nameAndData.data, 0o600); err != nil {
 			log.Fatalf("Failed to write file: %v", err)
 		}
 	}
