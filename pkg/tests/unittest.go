fa945f3ba477b39eec9375dcf0d48316ddc74b81
sahma19 <shd@energinet.dk>
2024-12-16 00:28:25 +0100

    Update unittest.go (#17)

--- pkg/tests/unittest.go
+++ pkg/tests/unittest.go
@@ -14,21 +14,23 @@ func RunUnitTests(testFiles []string) error {
 	var originalRules []*filenameAndData
 
 	for _, testFile := range testFiles {
-		unitTestInp, err := parseTestFile(testFile)
-		if err != nil {
-			return err
+		unitTestInp, readErr := parseTestFile(testFile)
+		if readErr != nil {
+			return readErr
 		}
 
 		for _, rulesFile := range unitTestInp.RuleFiles {
-			if err := processRulesFile(testFile, rulesFile, &originalRules); err != nil {
-				return err
+			processErr := processRulesFile(testFile, rulesFile, &originalRules)
+			if processErr != nil {
+				return processErr
 			}
 		}
 	}
 
-	if err := runPromtoolTests(testFiles); err != nil {
+	promtoolErr := runPromtoolTests(testFiles)
+	if promtoolErr != nil {
 		restoreOriginalFiles(originalRules)
-		return err
+		return promtoolErr
 	}
 
 	restoreOriginalFiles(originalRules)
@@ -36,78 +38,80 @@ func RunUnitTests(testFiles []string) error {
 }
 
 func parseTestFile(testFile string) (*unitTestFile, error) {
-	data, err := os.ReadFile(testFile)
-	if err != nil {
-		return nil, err
+	data, readErr := os.ReadFile(testFile)
+	if readErr != nil {
+		return nil, readErr
 	}
 
 	var unitTestInp unitTestFile
-	if err := yaml.Unmarshal(data, &unitTestInp); err != nil {
-		return nil, err
+	unmarshalErr := yaml.Unmarshal(data, &unitTestInp)
+	if unmarshalErr != nil {
+		return nil, unmarshalErr
 	}
-
 	return &unitTestInp, nil
 }
 
 func processRulesFile(testFile, rulesFile string, originalRules *[]*filenameAndData) error {
 	relativePath := fmt.Sprintf("%s/%s", filepath.Dir(testFile), rulesFile)
 
-	yamlData, err := os.ReadFile(relativePath)
-	if err != nil {
-		return err
+	yamlData, readErr := os.ReadFile(relativePath)
+	if readErr != nil {
+		return readErr
 	}
 
-	unstructured, err := unmarshalYAML(yamlData)
-	if err != nil {
-		return err
+	unstructured, unmarshalErr := unmarshalYAML(yamlData)
+	if unmarshalErr != nil {
+		return unmarshalErr
 	}
 
 	if spec, found := unstructured["spec"]; found {
-		if err := updateRulesFile(relativePath, yamlData, spec, originalRules); err != nil {
-			return err
+		updateErr := updateRulesFile(relativePath, yamlData, spec, originalRules)
+		if updateErr != nil {
+			return updateErr
 		}
 	} else {
 		log.Printf("No spec found in file %s", rulesFile)
 	}
-
 	return nil
 }
 
 func unmarshalYAML(data []byte) (map[interface{}]interface{}, error) {
 	unstructured := make(map[interface{}]interface{})
-	if err := yaml.Unmarshal(data, &unstructured); err != nil {
-		return nil, err
+	unmarshalErr := yaml.Unmarshal(data, &unstructured)
+	if unmarshalErr != nil {
+		return nil, unmarshalErr
 	}
 	return unstructured, nil
 }
 
 func updateRulesFile(filePath string, originalData []byte, spec interface{}, originalRules *[]*filenameAndData) error {
-	ruleFileContentWithoutMetadata, err := yaml.Marshal(spec)
-	if err != nil {
-		return err
+	ruleFileContentWithoutMetadata, marshalErr := yaml.Marshal(spec)
+	if marshalErr != nil {
+		return marshalErr
 	}
 
 	*originalRules = append(*originalRules, &filenameAndData{filePath, originalData})
 
-	if err := os.WriteFile(filePath, ruleFileContentWithoutMetadata, 0o600); err != nil {
-		return err
+	writeErr := os.WriteFile(filePath, ruleFileContentWithoutMetadata, 0o600)
+	if writeErr != nil {
+		return writeErr
 	}
-
 	return nil
 }
 
 func runPromtoolTests(testFiles []string) error {
 	promtoolArgs := append([]string{"test", "rules"}, testFiles...)
 	command := exec.Command("promtool", promtoolArgs...)
-	output, err := command.CombinedOutput()
+	output, execErr := command.CombinedOutput()
 	log.Printf("%s", output)
-	return err
+	return execErr
 }
 
 func restoreOriginalFiles(rules []*filenameAndData) {
 	for _, nameAndData := range rules {
-		if err := os.WriteFile(nameAndData.filename, nameAndData.data, 0o600); err != nil {
-			log.Fatalf("Failed to write file: %v", err)
+		writeErr := os.WriteFile(nameAndData.filename, nameAndData.data, 0o600)
+		if writeErr != nil {
+			log.Fatalf("Failed to write file: %v", writeErr)
 		}
 	}
 }
