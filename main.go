1c5ed01f8e0340913f1243c4c296e684e93a6c6c
sahma19 <shd@energinet.dk>
2024-12-16 03:31:18 +0100

    LINT (#18)

--- main.go
+++ main.go
@@ -8,11 +8,9 @@ import (
 )
 
 func main() {
-
 	if len(os.Args[1:]) == 0 {
 		log.Fatalf("Usage: po-test test-file1.yaml test-file2.yaml")
 	}
-
 	err := tests.RunUnitTests(os.Args[1:])
 	if err != nil {
 		log.Fatalf("Failed: %v", err)
